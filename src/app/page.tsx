833c271397be6b858a528734c9e3d3180c5af74a
Sagarr101 <sagarpandit0114@gmail.com>
2026-03-07 17:49:04 +0530

    Fix: add 'use client' to layout.tsx, correct iframe JSX in contact/page.tsx, fix parenthesis in page.tsx for Vercel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default async function Home() {
                   {item.value}
                 </div>
               </Card>
-          ))}
+            ))}
         </div>
       </section>
 
