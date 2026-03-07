7c4600c50e7768d3de82e364c7c3963c71964cd0
Sagarr101 <sagarpandit0114@gmail.com>
2026-03-07 17:32:03 +0530

    Fix Home page JSX error for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,6 @@ export default async function Home() {
                   {item.value}
                 </div>
               </Card>
-            </Link>
           ))}
         </div>
       </section>
